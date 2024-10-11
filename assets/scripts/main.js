e09964c32a5784a01c1e88b075327722556e3542
Gretchin Ogao <gretchin->
2024-10-11 21:22:18 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,15 @@
+const body = document.body
+//body.append("Hello", "Day 2")
+
+//const div = document.createElement("div")
+//body.append(div)
+
+//const strong = document.createElement("strong")
+//strong.innerText = "Hello"
+//div.append(strong)
+
+
+
+
+const div = document.querySelector("div")
+const spani = document.querySelector("#hi")
